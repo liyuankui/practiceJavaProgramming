2d802d757c0f2ddc4871e5c6a37d3c3834cc012d
Kyle Lee <liyuakui@gmail.com>
2012-09-23 17:03:37 +0800

    import works ap, and two more immutable data structures

--- src/lyk/practice/OutputNpowerN.java
+++ src/lyk/practice/OutputNpowerN.java
@@ -1,9 +1,6 @@
 package lyk.practice;
 
-import lyk.practice.datastructure.LinkedList;
-import lyk.practice.datastructure.LinkedNode;
-
-import java.util.*;
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,16 +12,16 @@ import java.util.*;
 public class OutputNpowerN {
 
 
-    static void generateNpowerN(String str){
+    static void generateNpowerN(String str) {
         // remove duplicate
 
-        char[] chars=str.toCharArray();
+        char[] chars = str.toCharArray();
         //sort
         Arrays.sort(chars);
         int n = chars.length;
-        int [] ptrs= new int[n+1];
-        while(ptrs[0]!=1){
-            for(int i=1;i<=n;i++){
+        int[] ptrs = new int[n + 1];
+        while (ptrs[0] != 1) {
+            for (int i = 1; i <= n; i++) {
                 System.out.print(chars[ptrs[i]]);
             }
             System.out.println();
@@ -32,16 +29,17 @@ public class OutputNpowerN {
             //increase
             ptrs[n]++;
             // carrying
-            for(int index=n;index>0; index--){
-                if(ptrs[index]==n){
-                    ptrs[index]=0;
-                    ptrs[index-1]++;
+            for (int index = n; index > 0; index--) {
+                if (ptrs[index] == n) {
+                    ptrs[index] = 0;
+                    ptrs[index - 1]++;
                 }
             }
         }
     }
-    public static void main(String [] args){
-       String str="abcd";
-       generateNpowerN(str);
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        generateNpowerN(str);
     }
 }
